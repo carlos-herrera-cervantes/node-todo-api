5243f26679990024fb77fb03ae4f5b9eb279e93e
Carlos Herrera <carlos.cervantes.cp@gmail.com>
2019-04-05 01:12:15 -0600

    Add test for DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -336,3 +336,22 @@ describe('POST /users/login', () => {
   });
   /** @endregion */
 });
+
+describe('DELETE /users/me/token', () => {
+  it('Should remove auth token on logout', done => {
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .end((error, response) => {
+      if (error) {
+        return done(error);
+      }
+
+      User.findById(users[0]._id).then(user => {
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch(error => done(error));
+    });
+  });
+});
